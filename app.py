3bcc50aee4378c95dba382bf4618e17f7ee0faf4
Raza Imam <razaimam9851@gmail.com>
2025-10-19 21:01:21 +0530

    Add images, update templates, new CSS for medium-size images

--- app.py
+++ app.py
@@ -2,52 +2,46 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Home page
+# ---------- ROUTES ----------
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# About page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# Nutrition page
 @app.route('/nutrition')
 def nutrition():
     return render_template('nutrition.html')
 
-# Motivation page
 @app.route('/motivation')
 def motivation():
     return render_template('motivation.html')
 
-# Supplements page
 @app.route('/supplements')
 def supplements():
     return render_template('supplements.html')
 
-# Workout page
 @app.route('/workout')
 def workout():
     return render_template('workout.html')
 
-# Blog page
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
 
-# Privacy Policy page
 @app.route('/privacy-policy')
 def privacy_policy():
     return render_template('privacy_policy.html')
 
-# Email subscription (dummy)
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
     email = request.form.get('email')
-    # Here you can add code to save email in database or send to mailing list
+    # In real projects, you would save this email in a database or email list
+    print(f"New subscriber: {email}")
     return redirect(url_for('home'))
 
-if __name__ == '__main__':
+# ---------- MAIN ----------
+if __name__ == "__main__":
     app.run(debug=True)
